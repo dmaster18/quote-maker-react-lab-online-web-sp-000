46ecb1590cedc9068eaa15e2067a207678b38139
David Strauch <dmaster18@gmail.com>
2021-01-23 23:01:49 +0000

    Automatically backed up by Learn

--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -11,8 +11,7 @@ export default (state = [], action) => {
     case 'UPVOTE_QUOTE':
       index = state.findIndex(quote => quote.id === action.quoteId);
       quote = state[index];
-      return [...state.slice(0, index), Object.assign({}, quote, { votes: quote.votes += 1 }), ...state.slice(index + 1)
-      ];
+      return [...state.slice(0, index), Object.assign({}, quote, { votes: quote.votes += 1 }), ...state.slice(index + 1)];
 
     case 'DOWNVOTE_QUOTE':
       index = state.findIndex(quote => quote.id === action.quoteId);
