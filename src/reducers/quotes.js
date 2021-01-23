5412b7acd1756177b382110c2377dd3c773a5beb
David Strauch <dmaster18@gmail.com>
2021-01-23 23:00:43 +0000

    Automatically backed up by Learn

--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -4,10 +4,8 @@ export default (state = [], action) => {
   let quote;
 
   switch (action.type) {
-
     case 'ADD_QUOTE':
       return state.concat(action.quote);
-
     case 'REMOVE_QUOTE':
       return state.filter(quote => quote.id !== action.quoteId);
 
