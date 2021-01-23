3a29f7357004c6b22e0856a99d132827386e87fe
David Strauch <dmaster18@gmail.com>
2021-01-23 23:00:53 +0000

    Automatically backed up by Learn

--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -7,7 +7,7 @@ export default (state = [], action) => {
     case 'ADD_QUOTE':
       return state.concat(action.quote);
     case 'REMOVE_QUOTE':
-      return state.filter(quote => quote.id !== action.quoteId);
+      return state.filter(quote => (quote.id !== action.quoteId));
 
     case 'UPVOTE_QUOTE':
       index = state.findIndex(quote => quote.id === action.quoteId);
